37b68ebba9680e96168043abd9a60afdaf3f7cea
Yash Kashte <72797164+Yashkashte5@users.noreply.github.com>
2024-07-03 21:14:12 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,47 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+    <center> 
+    <ul style="list-style: none">
+        <li><b>Project members</b></li>
+        <li>Yash Kashte</li>
+        <li>Samarth Salvi</li>
+        <li>Rugved Patil</li>
+        <li>Siddhesh Toraskar</li>
+    </ul></center>        
+            
+        </div>
+        </section>
+        <div id="preloader"></div>
+        <footer class=" py-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="mt-0 text-white">Contact us</h2>
+                        <hr class="divider my-4" />
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+                        <div class="text-white">547653452</div>
+                    </div>
+                    <div class="col-lg-4 mr-auto text-center">
+                        <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                        <!-- Make sure to change the email address in BOTH the anchor text and the link target below!-->
+                        <a class="d-block" href="mailto:vpadmin@gmail.com">vpadmin@gmail.com</a>
+                    </div>
+                </div>
+            </div>
